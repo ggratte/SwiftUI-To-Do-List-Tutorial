54fb099e2addf5cb8036bfa916978d01743eb035
Göran Gratte <goran.gratte@gmail.com>
2024-01-29 05:55:56 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Göran Gratte on 2024-01-29.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
