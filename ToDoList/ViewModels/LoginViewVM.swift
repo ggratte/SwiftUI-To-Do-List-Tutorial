51ffa299e20df35f5439ac387c2c52fb97452e5e
Göran Gratte <goran.gratte@gmail.com>
2024-01-30 05:38:54 +0100

    progress 1:02:40

--- ToDoList/ViewModels/LoginViewVM.swift
+++ ToDoList/ViewModels/LoginViewVM.swift
@@ -5,6 +5,7 @@
 //  Created by Göran Gratte on 2024-01-29.
 //
 
+import FirebaseAuth
 import Foundation
 
 class LoginViewVM: ObservableObject {
@@ -18,15 +19,24 @@ class LoginViewVM: ObservableObject {
     }
 
     func login(){
-        guard !email.trimmingCharacters(in: .whitespaces).isEmpty && !password.trimmingCharacters(in: .whitespaces).isEmpty else {
-            errorMessage = "please fill in all fields"
-            return
-        }
-
-        print("called1")
+        guard validate() else {return}
+        Auth.auth().signIn(withEmail: email, password: password)
     }
 
-    func validate() {
+    private func validate() -> Bool {
+
+        errorMessage = ""
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty && !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            errorMessage = "please fill in all fields"
+            return false
+        }
+
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "invalid email"
+            return false
+        }
+
+        return true
 
     }
 }
