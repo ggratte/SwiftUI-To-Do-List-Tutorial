e250d0afa0bc015034d6c57dc8ad825136225e91
Göran Gratte <goran.gratte@gmail.com>
2024-01-31 19:54:34 +0100

    Progress 2:37:20

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -14,10 +14,56 @@ struct ProfileView: View {
     var body: some View {
         NavigationView{
             VStack{
-
+                if let user = VM.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading...")
+                }
             }
             .navigationTitle("Profile")
+        }.onAppear{
+            VM.fetchUser()
         }
+    }
+
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        // Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            .frame(width: 125, height: 125)
+            .padding()
+
+
+        // Info: Name, Email, Member since
+        VStack(alignment: .leading) {
+            HStack{
+                Text("Name; ")
+                    .bold()
+                Text(user.name)
+            }.padding()
+            HStack{
+                Text("Email; ")
+                    .bold()
+                Text(user.email)
+            }.padding()
+            HStack{
+                Text("Member Since; ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }.padding()
+        }
+        .padding()
+
+        // Sign out
+        Button("Log Out") {
+            VM.logOut()
+        }
+        .tint(.red)
+        .padding()
+        Spacer()
     }
 }
 
